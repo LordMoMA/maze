1a2edee5ce160425543f1956a10742b7320740be
David Lee <lihongyi0512@gmail.com>
2023-02-27 21:44:20 +0800

    “maze”

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 
